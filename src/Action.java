524ce8b8d54e7c1b3621e5682eecd0a114aa9381
Randika Ujith <ujith169@gmail.com>
2023-01-22 23:30:28 +0000

    Bill area UI configured

--- src/Action.java
+++ src/Action.java
@@ -1,10 +1,20 @@
 public class Action{
     
-    PosGUI gui = new PosGUI();
+    private String bcDisplay;
 
     public Action(){
-        final String responce = gui.returnAction();
-        System.out.println(responce);
+    }
+
+    public void getResponse(String s){
+        if(s == "1" || s == "2" || s == "3" || s == "4" || s == "5" || s == "6" || s == "7" || s == "8" || s == "9" || s == "0" || s == "00" || s == "000"){
+            bcDisplay = s;
+        } else if(s == "Slush" || s == "Milk Shake" || s == "Snow Shock" || s == "HotChocolate" || s == "Ice Cream" || s == "Grocery" || s == "Alcohol" || s == "Lottery" || s == "PayPoint" || s == "VAPE" || s == "NEWS" || s == "Fruit/Veg"){
+            bcDisplay = null;
+        }
+    }
+
+    public String bcDisplay(){
+        return bcDisplay;
     }
     
 }
